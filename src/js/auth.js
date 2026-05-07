ed8dc912a432a7251becfb5233014f2c20a7aa48
farmacurapet <edsonoliveira27@hotmail.com>
2026-05-07 19:06:35 -0300

    Implementando estrutura de autenticação profissional

---
+++ src/js/auth.js
@@ -0,0 +1,13 @@
+﻿import { supabase } from './api/supabase.js';
+
+// Função para verificar se o usuário está logado
+async function checkUser() {
+    const { data: { user } } = await supabase.auth.getUser();
+    
+    // Se não estiver logado e não estiver na página de login, expulsa
+    if (!user && !window.location.href.includes('login.html')) {
+        window.location.href = '../auth/login.html';
+    }
+}
+
+checkUser();
