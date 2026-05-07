2fcfb0e698a7ca13e92c23d16b845b12218e597c
farmacurapet <edsonoliveira27@hotmail.com>
2026-05-07 19:08:06 -0300

    Sistema de login profissional implementado

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,13 +1,23 @@
 ﻿import { supabase } from './api/supabase.js';
 
-// Função para verificar se o usuário está logado
-async function checkUser() {
-    const { data: { user } } = await supabase.auth.getUser();
-    
-    // Se não estiver logado e não estiver na página de login, expulsa
-    if (!user && !window.location.href.includes('login.html')) {
-        window.location.href = '../auth/login.html';
-    }
-}
+const loginForm = document.getElementById('loginForm');
 
-checkUser();
+if (loginForm) {
+    loginForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        });
+
+        if (error) {
+            alert('Erro ao acessar: ' + error.message);
+        } else {
+            // Sucesso! Redireciona para a área logada (Dashboard)
+            window.location.href = '../dashboard/index.html';
+        }
+    });
+}
